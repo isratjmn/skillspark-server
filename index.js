b9a98693941b7cebeefdeff778edd33d5e8881c2
IsratMoon <mnisrat.j@gmail.com>
2023-07-23 13:23:45 +0600

    Add Testimonials API

--- index.js
+++ index.js
@@ -2,11 +2,66 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 require("dotenv").config();
-const port = process.env.PORT || 5000;
+const Testimonials = require("./testimonials.json");
+const port = process.env.PORT || 7000;
 
 // Middleware
 app.use(cors());
 app.use(express.json());
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ocimcqo.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+	serverApi: {
+		version: ServerApiVersion.v1,
+		strict: true,
+		deprecationErrors: true,
+	},
+});
+
+async function run() {
+	try {
+		client.connect();
+
+		// Collections Setup
+		const coursesCollection = client
+			.db("skillSparkDB")
+			.collection("courses");
+		const galleryCollection = client
+			.db("skillSparkDB")
+			.collection("gallery");
+
+		// Get Courses Data
+		app.get("/courses", async (req, res) => {
+			const result = await coursesCollection.find().toArray();
+			res.send(result);
+		});
+
+		// Cet Gallery Image
+		app.get("/gallery", async (req, res) => {
+			try {
+				const result = await galleryCollection.find().toArray();
+				res.send(result);
+			} catch (error) {
+				res.status(500).send({ error: error.message });
+			}
+		});
+
+		// Testimonials Get Api
+		app.get("/testimonials", (req, res) => {
+			res.send(Testimonials);
+		});
+
+		await client.db("admin").command({ ping: 1 });
+		console.log(
+			"Pinged your deployment. You successfully connected to MongoDB!"
+		);
+	} finally {
+		// await client.close();
+	}
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
 	res.send("SkillSpark Server is running.....");
